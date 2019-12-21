93ea232c5f19df040f6145c8c4ba9b0c4fa24a26
Mohammed Jawad <mohammed.jawad98@codeforiraq.org>
2019-12-21 19:05:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-/* Given Code, don't edit */
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
-function greet(timeString){
- let hour= timeString.split(":");
- if(hour[0]<12) return "Good Morning"
- else if(hour[0]>12 && hour[0]<17) return "Good Afternoon"
-   else if(hour[0]>17) return "Good Evening"
-}
-/* Write your implementation of displayMessage() */
-function displayMessage(greeting){
-  let h=document.getElementById("greeting");
-  h.innerText=greeting;
-}
+
 /* Write your implementation of greet() */
 
+function greet (timeString) {
+
+  if (parseInt(timeString.split(":").join('')) < 1200 ) {
+
+    return "Good Morning";
+  }
+    if (parseInt(timeString.split(":").join('') > 1200 && timeString.split(":").join('')) < 1700) {
+
+    return "Good Afternoon";
+  }
+    if (parseInt(timeString.split(":").join('')) >= 1700) {
+
+    return "Good Evening";
+  }
+
+
+}
+/* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
